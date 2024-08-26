73d60827e3d40fefb69bff5a4b0fe2c4ef7fe614
gchamb <gianttechambers@gmail.com>
2024-08-26 17:09:50 -0500

    feat: :sparkles: progress entry queries

---
+++ internal/database/queries/progress-entry.go
@@ -0,0 +1,40 @@
+package queries
+
+import (
+	"database/sql"
+	"egoist/internal/structs"
+
+	"github.com/google/uuid"
+)
+
+func (q *Queries) InsertProgressEntry(txn *sql.Tx, user structs.ProgressEntry) (error){
+	var query string 
+	
+	if txn == nil {
+		query = `INSERT INTO progress_entry (id, azure_blob_key, current_weight, user_id)
+        VALUES (:id, :azure_blob_key, :current_weight, :user_id)`
+		_, err := q.DB.NamedExec(query, user)
+		return err
+	}	
+	
+	query = `INSERT INTO progress_entry (id, azure_blob_key, current_weight, user_id)
+        VALUES (?, ?, ?, ?)`
+
+	_, err := txn.Exec(query, user.ID, user.AzureBlobKey, user.CurrentWeight, user.UserID);
+	return err
+}
+
+func (q *Queries) CreateProgressEntry(txn *sql.Tx, entry structs.ProgressEntry) (string, error) {
+	id, err := uuid.NewUUID()
+	if err != nil {
+		return "", err
+	}
+	entry.ID = id.String()
+
+	err = q.InsertProgressEntry(txn, entry)
+	if err != nil {
+		return "", err
+	}
+
+	return id.String(), err
+}
