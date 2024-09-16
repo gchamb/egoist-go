c89141ebc54b850e73bfb36516c876533295f434
gchamb <gianttechambers@gmail.com>
2024-09-16 16:08:00 -0500

    refactor: :recycle: inject connection into aws controller

--- app/routes/aws.go
+++ app/routes/aws.go
@@ -1,15 +1,16 @@
 package routes
 
 import (
+	"egoist/app"
 	"egoist/app/controllers"
 	"egoist/app/middlewares"
 
 	"github.com/go-chi/chi/v5"
 )
 
-func RegisterAWSRoutes(r chi.Router) {
+func RegisterAWSRoutes(r chi.Router, global *app.Globals) {
 	r.Group(func(r chi.Router) {
 		r.Use(middlewares.AuthenticateJWT)
-		r.Get("/aws/upload", controllers.GenerateUploadPresignedUrl)
+		r.Get("/aws/upload", controllers.GenerateUploadPresignedUrl(global))
 	})
 }
