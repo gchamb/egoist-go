4d66d230493c9e35bcf0db9ff03556437f71174a
gchamb <gianttechambers@gmail.com>
2024-09-16 16:30:06 -0500

    refactor: :recycle: inject connection into entry

--- app/controllers/entry.go
+++ app/controllers/entry.go
@@ -2,8 +2,7 @@ package controllers
 
 import (
 	"database/sql"
-	"egoist/internal/database"
-	"egoist/internal/database/queries"
+	"egoist/app"
 	"egoist/internal/structs"
 	"encoding/json"
 	"fmt"
@@ -11,55 +10,54 @@ import (
 	"time"
 )
 
-func PutEntry(w http.ResponseWriter, r *http.Request) {
-	uid := r.Context().Value("uid").(string)
+func PutEntry(global *app.Globals) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		uid := r.Context().Value("uid").(string)
 
-	var requestBody structs.PutAssetRequest
-	decoder := json.NewDecoder(r.Body)
-	decoder.Decode(&requestBody)
+		var requestBody structs.PutAssetRequest
+		decoder := json.NewDecoder(r.Body)
+		decoder.Decode(&requestBody)
 
-	if err := requestBody.ValidPutAssetRequest(); err != nil {
-		fmt.Println(err.Error())
-		w.WriteHeader(http.StatusBadRequest)
-		return
+		if err := requestBody.ValidPutAssetRequest(); err != nil {
+			fmt.Println(err.Error())
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		txn, err := global.Queries.DB.BeginTx(r.Context(), &sql.TxOptions{})
+		defer txn.Commit()
+
+		if err != nil {
+			fmt.Println(err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		location, err := time.LoadLocation(requestBody.Timezone)
+
+		if err != nil {
+			fmt.Println(err.Error())
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		todaysDateInTimezone := time.Now().In(location)
+		date := fmt.Sprintf("%d-%d-%d", todaysDateInTimezone.Year(), todaysDateInTimezone.Month(), todaysDateInTimezone.Day())
+
+		entry := structs.ProgressEntry{BlobKey: requestBody.Key, CurrentWeight: requestBody.CurrentWeight, UserID: uid, CreatedAt: date}
+
+		if _, err := global.Queries.CreateProgressEntry(txn, entry); err != nil {
+			fmt.Println(err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		if err := global.Queries.UpdateUser(txn, r.Context(), structs.UpdateUserRequest{CurrentWeight: &requestBody.CurrentWeight}, uid); err != nil {
+			fmt.Println(err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
 	}
-
-	db := database.ConnectDB()
-	queries := queries.New(db)
-	
-	txn, err := queries.DB.BeginTx(r.Context(), &sql.TxOptions{})
-	defer txn.Commit()
-
-	if err != nil {
-		fmt.Println(err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	location, err := time.LoadLocation(requestBody.Timezone);
-
-	if err != nil {
-		fmt.Println(err.Error())
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	todaysDateInTimezone := time.Now().In(location)
-	date := fmt.Sprintf("%d-%d-%d",todaysDateInTimezone.Year(), todaysDateInTimezone.Month(), todaysDateInTimezone.Day())
-
-	entry := structs.ProgressEntry{BlobKey: requestBody.Key, CurrentWeight: requestBody.CurrentWeight, UserID: uid, CreatedAt: date }
-	
-	if _, err := queries.CreateProgressEntry(txn, entry); err != nil {
-		fmt.Println(err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	if err := queries.UpdateUser(txn, r.Context(), structs.UpdateUserRequest{CurrentWeight: &requestBody.CurrentWeight}, uid); err != nil {
-		fmt.Println(err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
 }
--- app/routes/entry.go
+++ app/routes/entry.go
@@ -1,15 +1,16 @@
 package routes
 
 import (
+	"egoist/app"
 	"egoist/app/controllers"
 	"egoist/app/middlewares"
 
 	"github.com/go-chi/chi/v5"
 )
 
-func RegisterEntryRoutes(r chi.Router) {
+func RegisterEntryRoutes(r chi.Router, global *app.Globals) {
 	r.Group(func(r chi.Router) {
 		r.Use(middlewares.AuthenticateJWT)
-		r.Put("/entry", controllers.PutEntry)
+		r.Put("/entry", controllers.PutEntry(global))
 	})
 }
