dc567dc9f6891b701df6efb6081c4212e560af5e
gchamb <gianttechambers@gmail.com>
2024-09-16 16:30:29 -0500

    refactor: :recycle: inject connection into revenue cat

--- app/routes/revenue-cat.go
+++ app/routes/revenue-cat.go
@@ -1,15 +1,16 @@
 package routes
 
 import (
+	"egoist/app"
 	"egoist/app/controllers"
 	"egoist/app/middlewares"
 
 	"github.com/go-chi/chi/v5"
 )
 
-func RegisterRevenueCatRoutes(r chi.Router){
+func RegisterRevenueCatRoutes(r chi.Router, global *app.Globals) {
 	r.Group(func(r chi.Router) {
 		r.Use(middlewares.AuthenticateWebhook)
-		r.Post("/revenue-cat/webhook", controllers.RevenueCatWebhook)
-	})	
+		r.Post("/revenue-cat/webhook", controllers.RevenueCatWebhook(global))
+	})
 }
