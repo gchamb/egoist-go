1d245242304153c6e3108bf21d943105ce6c7b2c
gchamb <gianttechambers@gmail.com>
2024-09-16 11:46:42 -0500

    feat: :sparkles: completed updating user

--- app/controllers/user.go
+++ app/controllers/user.go
@@ -66,3 +66,46 @@ func OnboardUser(w http.ResponseWriter, r *http.Request){
 	
 	w.WriteHeader(http.StatusOK)
 }
+
+func UpdateUser(w http.ResponseWriter, r *http.Request){
+	uid := r.Context().Value("uid").(string)
+
+	// get the request body
+	var requestBody struct {
+		GoalWeight    *float32 `json:"goal_weight"`
+		CurrentWeight *float32 `json:"current_weight"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	decoder.Decode(&requestBody)
+	fmt.Println(requestBody)
+
+	if requestBody.GoalWeight == nil && requestBody.CurrentWeight == nil{
+		fmt.Println("goal weight and current weight can't be empty")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if requestBody.GoalWeight != nil && (*requestBody.GoalWeight < 70 || *requestBody.GoalWeight > 500) {
+		fmt.Println("invalid goal weight")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if requestBody.CurrentWeight != nil && (*requestBody.CurrentWeight < 70 || *requestBody.CurrentWeight > 500) {
+		fmt.Println("invalid current weight")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	db := database.ConnectDB()
+	queries := queries.New(db)
+
+	updateUser := structs.UpdateUserRequest{GoalWeight: requestBody.GoalWeight, CurrentWeight: requestBody.CurrentWeight}
+	if err := queries.UpdateUser(nil, r.Context(), updateUser , uid); err != nil {
+		fmt.Println(err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
